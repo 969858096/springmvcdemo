10568d8a5a9237fe36606bb1c15e9084a601c31e
lsp <969858096@qq.com>
2019-12-07 16:35:36 +0800

    添加测试工具

---
+++ demo1/src/main/java/utils/UtilsDemo1.java
@@ -0,0 +1,13 @@
+package utils;
+
+/**
+ * @FileName: UtilsDemo1
+ * @Description:
+ * @AuthOr: lsp
+ * @Date: 2019/12/7 16:35
+ */
+public class UtilsDemo1 {
+    public static void main(String[] args) {
+        System.out.println("测试工具");
+    }
+}
