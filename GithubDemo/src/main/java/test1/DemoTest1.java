d95c885fcaeaaab85ec2000a726691b11f563a47
lsp <969858096@qq.com>
2019-12-07 16:51:24 +0800

    测试提交代码到远程库1

---
+++ GithubDemo/src/main/java/test1/DemoTest1.java
@@ -0,0 +1,13 @@
+package test1;
+
+/**
+ * @FileName: DemoTest1
+ * @Description:
+ * @AuthOr: lsp
+ * @Date: 2019/12/7 16:50
+ */
+public class DemoTest1 {
+    public static void main(String[] args) {
+        System.out.println("测试添加1");
+    }
+}
