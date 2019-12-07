bcfac1ea81648b7ce17cd8f68b266b92d23f8b94
lsp <969858096@qq.com>
2019-12-07 15:19:46 +0800

    添加测试1

---
+++ GithubDemo/src/main/java/demo/Demo1.java
@@ -0,0 +1,14 @@
+package demo;
+
+/**
+ * @FileName: Demo1
+ * @Description:
+ * @AuthOr: lsp
+ * @Date: 2019/12/7 15:12
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("测试1");
+        System.out.println("测试2");
+    }
+}
