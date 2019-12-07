b9b2a051820670ec6c95eb7b48a0192de74c931e
lsp <969858096@qq.com>
2019-12-07 16:30:10 +0800

    1

--- demo1/src/main/java/test/Demo1.java
+++ demo1/src/main/java/test/Demo1.java
@@ -1,14 +1,13 @@
-package demo;
+package test;
 
 /**
  * @FileName: Demo1
  * @Description:
  * @AuthOr: lsp
- * @Date: 2019/12/7 15:12
+ * @Date: 2019/12/7 16:28
  */
 public class Demo1 {
     public static void main(String[] args) {
-        System.out.println("测试1");
-        System.out.println("测试2");
+        System.out.println("测试");
     }
 }
